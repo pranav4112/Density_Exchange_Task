93df48f6b5ef7f8efe5434578d664aa14e7f833b
Pranav <pranav.shastri04@gmail.com>
2023-12-02 00:07:07 +0530

    Listed workcards

---
+++ components/WorkWithUs/WorkCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WorkCard = ({heading,content}) => {
+  return (
+    <div className='flex flex-col p-4 md:w-96 w-44 md:h-56 bg-white rounded-xl gap-4 flex-shrink-0 md:mb-6 mr-6 hover:bg-[#fcfbff] hover:text-white duration-300 drop-shadow-xl'>
+      <h2 className='text-black md:text-lg text-sm font-bold  '>{heading}</h2>
+      <p className='text-[#363636] md:text-sm text-xs font-semibold'>{content}</p>
+      
+    </div>
+  )
+}
+
+export default WorkCard
