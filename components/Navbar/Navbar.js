6267c7bc72b2ba202981da0ba00b676a9b4539bb
Pranav <pranav.shastri04@gmail.com>
2023-12-01 17:58:15 +0530

    Navbar done

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+const Navbar = () => {
+  return (
+    <>
+      <motion.nav 
+        initial={{ y: -20, opacity: 0 }}
+        transition={{
+          delay: 0.1,
+          duration: 0.3
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        viewport={{once:true}}
+        className='md:px-56 px-6 flex justify-between items-center w-full pt-2 '>
+
+        <Image
+        src='/Images/logo.png'
+        width={55}
+        height={55}
+        alt="Logo"
+        style={{cursor:'pointer'}}
+        />
+
+      <div className='md:flex justify-center items-center hidden'>
+        <a className='mr-10 font-semibold text-[#5e5e5e]' href="./">Emotions</a>
+        <a className='mr-10 font-semibold text-[#5e5e5e]' href="./">Manifesto</a>
+        <a className='mr-10 font-semibold text-[#5e5e5e]' href="./">Self-Awareness Test</a>
+        <a className='mr-10 font-semibold text-[#5e5e5e]' href="./">Work With Us</a>
+
+      </div>
+
+      <motion.button 
+      initial={{ scale:0, opacity: 0 }}
+      transition={{
+        delay: 0.15,
+        duration: 0.4
+      }}
+      whileInView={{ scale: 1,opacity: 1 }}
+      viewport={{once:true}}
+      className='flex justify-center items-center bg-[#212020] duration-300 rounded-[50px] text-[#e6e4e4] px-5 py-3 font-medium cursor-pointer shadow-sm shadow-gray-500'>
+        Download app
+      </motion.button>
+
+      </motion.nav>
+    </>
+  )
+}
+
+export default Navbar
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 'use client'
-import AheadApp from "@/components/AheadApp"
-import BestEQ from "@/components/BestEQ"
-import EQ_IQ from "@/components/EQ_IQ"
-import Features from "@/components/Features"
-import Footer from "@/components/Footer"
-import Hero from "@/components/Hero"
-import Improvement from "@/components/Improvement"
-import Navbar from "@/components/Navbar"
-import Signature from "@/components/Signature"
-import Vacancies from "@/components/Vacancies"
-import Wondered from "@/components/Wondered"
-import WorkWithUs from "@/components/WorkWithUs"
+import AheadApp from "@/components/AheadApp/AheadApp"
+import BestEQ from "@/components/BestEQ/BestEQ"
+import EQ_IQ from "@/components/EQ_IQ/EQ_IQ"
+import Features from "@/components/Features/Features"
+import Footer from "@/components/Footer/Footer"
+import Hero from "@/components/Hero/Hero"
+import Improvement from "@/components/Improvement/Improvement"
+import Navbar from "@/components/Navbar/Navbar"
+import Signature from "@/components/Signature/Signature"
+import Vacancies from "@/components/Vacancies/Vacancies"
+import Wondered from "@/components/Wondered/Wondered"
+import WorkWithUs from "@/components/WorkWithUs/WorkWithUs"
 
 export default function Home() {
   return (
